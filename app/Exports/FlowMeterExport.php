5cc47532a9a86d810557771ca79c5b08c036bbfa
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-06-25 14:10:41 +0700

    export flowmeter

--- app/Exports/FlowMeterExport.php
+++ app/Exports/FlowMeterExport.php
@@ -19,9 +19,10 @@ class FlowMeterExport implements FromQuery, WithHeadings
     {
         return [
             'Date',
-            'Trx',
+            'Shift',
+            'Fuel Tank/SADP',
             'Status',
-            'Fuel Tank',
+            'To',
             'Flowmeter Start',
             'Flowmeter End',
             'Sounding Start',
@@ -39,9 +40,13 @@ class FlowMeterExport implements FromQuery, WithHeadings
 
         return FlowMeter::selectRaw('
                 flow_meters.date AS date,
-                IF (flow_meters.flowmeter_start - flow_meters.flowmeter_end > 0 , "IN", "OUT") as trx,
-                flow_meters.status AS status,
-                fuel_tanks.name AS fuel_tank,
+                flow_meters.shift AS shift,
+                CASE
+                    WHEN flow_meters.fuel_tank_id > 0 THEN fuel_tanks.name
+                    WHEN flow_meters.sadp_id > 0 THEN sadps.name
+                END,
+                IF(flow_meters.status = "S", "Stock Awal", "Transfer"),
+                t.name AS transfer_to,
                 flow_meters.flowmeter_start AS flowmeter_start,
                 flow_meters.flowmeter_end AS flowmeter_end,
                 flow_meters.sounding_start AS sounding_start,
@@ -50,7 +55,10 @@ class FlowMeterExport implements FromQuery, WithHeadings
                 flow_meters.volume_by_sounding AS volume_by_sounding,
                 flow_meters.volume_by_sounding - (flow_meters.flowmeter_end - flowmeter_start) AS selisih
             ')
-            ->join('fuel_tanks', 'fuel_tanks.id', '=', 'flow_meters.fuel_tank_id')
+            ->join('fuel_tanks', 'fuel_tanks.id', '=', 'flow_meters.fuel_tank_id', 'LEFT')
+            ->join('fuel_tanks AS t', 't.id', '=', 'flow_meters.transfer_to_fuel_tank_id', 'LEFT')
+            ->join('sadps', 'sadps.id', '=', 'flow_meters.sadp_id', 'LEFT')
+            ->join('users', 'users.id', '=', 'flow_meters.user_id', 'LEFT')
             ->when($request, function($query) use ($request) {
                 return $query->whereRaw("`date` BETWEEN '{$request->from}' AND '{$request->to}'");
             })
