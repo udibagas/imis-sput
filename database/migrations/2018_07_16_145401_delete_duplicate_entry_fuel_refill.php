db6590f39686ae0eb976b0734a188d62e3ac9e5f
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-07-16 15:20:37 +0700

    auto delete duplicate entry

---
+++ database/migrations/2018_07_16_145401_delete_duplicate_entry_fuel_refill.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\FuelRefill;
+
+class DeleteDuplicateEntryFuelRefill extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fuel_refills', function (Blueprint $table) {
+            // pilih dari tanggal 1
+            $fuelRefills = FuelRefill::whereRaw('`date` >= "2018-07-01"')->orderBy('id', 'ASC');
+
+            foreach ($fuelRefills as $f) {
+                $exists = App\FuelRefill::selectRaw('GROUP_CONCAT(id) AS ids')
+                    ->where('date', $f->date)
+                    ->where('shift', $f->shift)
+                    ->where('fuel_tank_id', $f->fuel_tank_id)
+                    ->where('unit_id', $f->unit_id)
+                    ->where('employee_id', $f->employee_id)
+                    ->where('km', $f->km)
+                    ->orderBy('id', 'ASC')
+                    ->first();
+
+                $idToBeDeleted = explode(',', $exists->ids);
+                unset($idToBeDeleted[0]);
+                FuelRefill::whereIn('id', $idToBeDeleted)->delete();
+            };
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
