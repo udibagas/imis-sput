d62871ea7660ec334c775aefba334abb7e6e9825
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-08-08 11:19:15 +0700

    upload barging

--- app/Barging.php
+++ app/Barging.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barging extends Model
 {
+    public const STATUS_START = 0;
+    public const STATUS_LOADING = 1;
+    public const STATUS_BREAKOWN = 2;
+    public const STATUS_DELAY = 3;
+    public const STATUS_IDLE = 4;
+    public const STATUS_COMPLETE = 5;
+
     protected $fillable = [
         'start', 'stop', 'jetty_id', 'barge_id',
         'buyer_id', 'volume', 'progress', 'status', 'description',
-        'customer_id', 'tugboat_id'
+        'customer_id', 'tugboat_id', 'volume_by_bucket_ctrl'
     ];
 
     protected $with = ['bargingMaterial'];
@@ -20,5 +27,17 @@ class Barging extends Model
 
     public function dwellingTime() {
         return $this->hasMany(DwellingTime::class);
+    }
+
+    public static function getStatusList()
+    {
+        return [
+            ['id' => self::STATUS_START, 'text' => 'Initiate', 'color' => 'info'],
+            ['id' => self::STATUS_LOADING, 'text' => 'Loading', 'color' => 'success'],
+            ['id' => self::STATUS_BREAKOWN, 'text' => 'Breakdown', 'color' => 'danger'],
+            ['id' => self::STATUS_DELAY, 'text' => 'Delay', 'color' => 'warning'],
+            ['id' => self::STATUS_IDLE, 'text' => 'Idle', 'color' => 'default'],
+            ['id' => self::STATUS_COMPLETE, 'text' => 'Complete', 'color' => 'primary'],
+        ];
     }
 }
